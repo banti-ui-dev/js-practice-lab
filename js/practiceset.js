eb4d6b5e18bd7494edba157169a0b081475e4b68
Banti Kumar <bantikumarsoni@gmail.com>
2026-02-25 15:32:14 +0530

    Some JS practice set trending Interview questions

---
+++ js/practiceset.js
@@ -0,0 +1,110 @@
+const a = {};               //{}.toString()  "[object Object]"
+const b = { key: "b" };
+const c = { key: "c" };
+a[b] = 123;                 // a["[object Object]"] = 123;
+a[c] = 456;                 // a["[object Object]"] = 456;
+// a[c] overwrite the value of a[b]
+console.log(a[b]);
+// result 456
+
+
+
+// show 1-20 numbers the num divided by 3 : Fizz, the num divided by  : Buzz and the num divided by both : Fizz Buzz
+const numA = (val) => {
+    const result = []
+    for (let i = 1; i <= val; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("Fizz Buzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i);
+        }
+    }
+    return result
+}
+console.log(numA(20));
+
+
+// write a function where pass a parameter r, write logic in that function that respond only even number between 0 to r
+const evenNum = (r) => {
+    const num = []
+    for (let i = 0; i <= r; i++) {
+        if (i % 2 === 0) {
+            num.push(i)
+        }
+    }
+    return num
+}
+console.log(evenNum(10));
+
+// we have a array check a number having in this array print Yes and if not print No
+const arr = [5, 6, 8, 6, 2, 1, 5, 8];
+const testAvail = (k, arr) => {
+    return arr.includes(k) ? "Yes" : "No"
+}
+console.log(testAvail(5, arr));
+
+//Return sum of even numbers from 1 to n.
+const sumEvennum = (n) => {
+    var sum = 0;
+    for (let i = 0; i <= n; i++) {
+        if (i % 2 === 0) {
+            sum += i
+        }
+    }
+    return sum
+}
+console.log(sumEvennum(520));
+
+//Find the Largest Number in Array (without Math.max)
+const arrA = [5, 6, 8, 6, 2, 1, 5, 8];
+const largestNum = (arrs) => {
+    let largest = -Infinity
+    let secLargest = -Infinity
+    for (let i = 0; i <= arrs.length; i++) {
+        if (arrs[i] > largest) {
+            largest = arrs[i]
+            arrs[i] = secLargest
+        } else if (arrs[i] > secLargest && arrs[i] !== largest) {
+            secLargest = arrs[i]
+        }
+    }
+    return [largest, secLargest]
+}
+console.log(largestNum(arrA));
+
+
+
+//Move all zeros to end (without changing order)
+const num = [0, 1, 0, 3, 12];
+const newNum = (abc) => {
+    let numarr = []
+    let zeroarr = []
+    for (let i = 0; i <= abc.length - 1; i++) {
+        if (abc[i] !== 0) {
+            numarr.push(abc[i])
+        } else {
+            zeroarr.push(abc[i])
+        }
+    }
+    return [...numarr, ...zeroarr]
+}
+console.log(newNum(num));
+
+
+//Find First Non-Repeating Character - programming
+const firstNonrep = (str) => {
+    let repeted = ""
+    for (let i = 0; i <= str.length; i++){
+       let char = str[i]
+       if(!repeted[char]){
+        return char
+       }
+    }    
+}
+console.log(firstNonrep("programming"));
+
+
