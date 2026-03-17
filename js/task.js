f93be0ef07ca5b84c262b54adfe837cf85bd5de6
Banti Kumar <bantikumarsoni@gmail.com>
2026-03-17 21:23:24 +0530

    Diagonal Difference (matrix problem)

--- js/task.js
+++ js/task.js
@@ -22,4 +22,19 @@ const sum = arr.reduce((a, b) => a + b, 0);
 console.log(total - sum); // 4
 
 
-//
+// result 11+5-12 = 4 and 4+5+10 = 19 the Difference:  19-4 = 15
+
+
+const arrN = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+const diagonalDiffrence = (arr) => {
+  let sumleftRight = 0
+  let sumrightLeft = 0
+  for (let i = 0; i < arr.length; i++) {    
+    let leftRight = arr[i][i]
+    let rightLeft = arr[i][arr.length-1-i]
+    sumleftRight = sumleftRight + leftRight
+    sumrightLeft = sumrightLeft + rightLeft
+  }
+  return Math.abs(sumleftRight-sumrightLeft)
+}
+console.log(diagonalDiffrence(arrN))
